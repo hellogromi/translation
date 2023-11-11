7d231ab6f4ccbc455f892bbf83fef06aae4ffab6
hellogromi <150126799+hellogromi@users.noreply.github.com>
2023-11-11 20:49:52 +0300

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -1,30 +1,30 @@
-print("the currency is USA dollars")
-print("Which sphere will you choose")
+print("The currency is USA dollars.")
+print("Which sphere will you choose?")
 print("· electricity (e)")
 print("· hot water (hw)")
 print("· cold water (cw)")
 print("· water drain (wd)")
 print("· gas (g)")
 print("· enter your tariff")
-print("you are 18 years old?")
-b = input("yes or no:", )
+print("You are 18 years old?")
+b = input("Enter yes or no:", )
 while b != "no":
-    sphere = input("enter the sphere:", )
-    tariff_1 = input("enter the tariff:", )
+    sphere = input("Enter the sphere:", )
+    tariff_1 = input("Enter the tariff:", )
     if "." in tariff_1:
         tariff = float(tariff_1)
     else:
         tariff = int(tariff_1)
     if sphere == "e":
-        print("How much kilowatt per hour have you used up")
+        print("How much kilowatt per hour have you used up?")
     elif sphere == "hw":
-        print("How much cubic meters gave you used up")
+        print("How much cubic meters gave you used up?")
     elif sphere == "cw":
-        print("How much cubic meters gave you used up")
+        print("How much cubic meters gave you used up?")
     elif sphere == "wd":
-        print("How much cubic meters have you used up in total")
+        print("How much cubic meters have you used up in total?")
     elif sphere == "g":
-        print("How much cubic meters gave you used up")
+        print("How much cubic meters gave you used up?")
     expenses = int(input("enter the expenses:", ))
     if sphere == "e":
         print("you have to pay:", tariff * expenses, "$")
@@ -37,13 +37,12 @@ while b != "no":
     elif sphere == "g":
         print("you have to pay:", tariff * expenses, "$")
     print("Do you want to transfer to another currency?")
-    answer = input("print yes or no:", )
+    answer = input("enter yes or no:", )
     a = tariff * expenses
     if answer == "yes":
-        print("Enter how much one dollar is worth in your currency")
+        print("Enter how much one dollar is worth in your currency.")
         cost = int(input())
         print(cost * a)
-    print("Would you like to continue")
-    b = input("yes or no:", )
-print("see you!")
-    
+    print("Would you like to continue?")
+    b = input("Enter yes or no:", )
+print("See you!")
